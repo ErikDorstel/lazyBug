46b0a25ecef3776ccffaf1ad1396a715b329dcf8
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-11-28 20:30:14 +0100

    add tilt average method

--- tilt.h
+++ tilt.h
@@ -8,25 +8,32 @@ Adafruit_MPU6050 mpu;
 struct tiltStruct { float x; float y; float cax; float cay; float caz; };
 struct tiltStruct tilt;
 
-float wrap360(float value) {
+float wrap180(float value) {
   while (value>+180) { value-=360; }
   while (value<-180) { value+=360; }
   return value; }
 
 void calibrateTilt() {
-  sensors_event_t a,g,t;
-  float ax=0; float ay=0; float az=0;
-  for (int x=0;x<20;x++) { mpu.getEvent(&a,&g,&t);
-    ax+=a.acceleration.x; ay+=a.acceleration.y; az+=a.acceleration.z; delay(200); }
-  tilt.cax=ax/20.0; tilt.cay=ay/20.0; tilt.caz=az/20.0; }
+  sensors_event_t a,g,t; float ax=0; float ay=0; float az=0;
+  for (int x=0;x<50;x++) { mpu.getEvent(&a,&g,&t);
+    ax+=a.acceleration.x; ay+=a.acceleration.y; az+=a.acceleration.z; delay(100); }
+  tilt.cax=ax/50.0; tilt.cay=ay/50.0; tilt.caz=az/50.0; }
 
 void getTilt() {
   sensors_event_t a,g,t; mpu.getEvent(&a,&g,&t);
   float ax=a.acceleration.x-tilt.cax;
   float ay=a.acceleration.y-tilt.cay;
   float az=a.acceleration.z+(9.81-tilt.caz);
-  tilt.x=wrap360((+atan2(ay,sqrt(az*az+ax*ax)))*57.29577951308232);
-  tilt.y=wrap360((-atan2(ax,sqrt(az*az+ay*ay)))*57.29577951308232); }
+  tilt.x=wrap180((+atan2(ay,sqrt(az*az+ax*ax)))*57.29577951308232);
+  tilt.y=wrap180((-atan2(ax,sqrt(az*az+ay*ay)))*57.29577951308232); }
+
+void getTiltAvg() {
+  sensors_event_t a,g,t; float ax=0; float ay=0; float az=0;
+  for (int x=0;x<50;x++) { mpu.getEvent(&a,&g,&t);
+    ax+=a.acceleration.x; ay+=a.acceleration.y; az+=a.acceleration.z; delay(10); }
+  ax=ax/50.0-tilt.cax; ay=ay/50.0-tilt.cay; az=az/50.0+(9.81-tilt.caz);
+  tilt.x=wrap180((+atan2(ay,sqrt(az*az+ax*ax)))*57.29577951308232);
+  tilt.y=wrap180((-atan2(ax,sqrt(az*az+ay*ay)))*57.29577951308232); }
 
 void initTilt() {
   I2Ctwo.begin(17,16); //SDA 17,SCL 16
